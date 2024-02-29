45b82204a4321bc38cae76bfb9560d37338ff546
Annubhuti Chatterjee <89660859+chatterjeeannubhuti1704@users.noreply.github.com>
2024-02-29 13:34:19 +0530

    first

---
+++ myscript.js
@@ -0,0 +1,179 @@
+change(1);
+function change(num){
+   if(num==1){
+    document.getElementById("main").style.backgroundImage="url(img/BG.png)";
+    document.getElementById("main").style.backgroundColor="#fddfc8";
+    document.getElementById("title").innerHTML="Smoked Salmon";
+    document.getElementById("details").innerHTML="Wild salmon is preferable as it contains higher amounts of omega-3 fatty acids and generally has less exposure to contaminants such as pollutants, antibiotics etc."
+    document.getElementById("ellipse").style.backgroundColor=" #f58a38";
+
+    document.getElementById("Pl1").classList.add("default");
+   
+    document.getElementById("Pl2").classList.add("bottom-left")
+  
+    document.getElementById("Pl3").classList.add("bottom-right")
+    
+    document.getElementById("Pl4").classList.add("top-right")
+
+
+
+    document.getElementById("Pl2").classList.remove("default");
+   
+    document.getElementById("Pl3").classList.remove("bottom-left")
+  
+    document.getElementById("Pl4").classList.remove("bottom-right")
+    
+    document.getElementById("Pl1").classList.remove("top-right")
+
+    document.getElementById("Pl3").classList.remove("default");
+   
+    document.getElementById("Pl4").classList.remove("bottom-left")
+  
+    document.getElementById("Pl1").classList.remove("bottom-right")
+    
+    document.getElementById("Pl2").classList.remove("top-right")
+
+    document.getElementById("Pl4").classList.remove("default");
+   
+    document.getElementById("Pl1").classList.remove("bottom-left")
+  
+    document.getElementById("Pl2").classList.remove("bottom-right")
+    
+    document.getElementById("Pl3").classList.remove("top-right")
+
+
+}
+   else if(num==2)
+   {
+    document.getElementById("main").style.backgroundImage="url(img/BG2.png)";
+    document.getElementById("main").style.backgroundColor="#ffdfce";
+    document.getElementById("title").innerHTML="Andong Jjimdak";
+    document.getElementById("details").innerHTML="Andong jjimdak is a South Korean dish consisting of braised chicken that is doused in a sweet and subtly spicy sauce which is infused with honey and soy sauce."
+    document.getElementById("ellipse").style.backgroundColor=" #fc6122";
+
+    document.getElementById("Pl2").classList.add("default");
+   
+    document.getElementById("Pl3").classList.add("bottom-left")
+  
+    document.getElementById("Pl4").classList.add("bottom-right")
+    
+    document.getElementById("Pl1").classList.add("top-right")
+
+
+
+
+    document.getElementById("Pl1").classList.remove("default");
+   
+    document.getElementById("Pl2").classList.remove("bottom-left");
+  
+    document.getElementById("Pl3").classList.remove("bottom-right");
+    
+    document.getElementById("Pl4").classList.remove("top-right");
+
+    document.getElementById("Pl3").classList.remove("default");
+   
+    document.getElementById("Pl4").classList.remove("bottom-left")
+  
+    document.getElementById("Pl1").classList.remove("bottom-right")
+    
+    document.getElementById("Pl2").classList.remove("top-right")
+
+    document.getElementById("Pl4").classList.remove("default");
+   
+    document.getElementById("Pl1").classList.remove("bottom-left")
+  
+    document.getElementById("Pl2").classList.remove("bottom-right")
+    
+    document.getElementById("Pl3").classList.remove("top-right")
+
+   }
+   else if(num==3)
+   {
+    document.getElementById("main").style.backgroundImage="url(img/BG3.png)";
+    document.getElementById("main").style.backgroundColor="#e1e9c3";
+    document.getElementById("title").innerHTML="Steak tartare";
+    document.getElementById("details").innerHTML="It is usually served with onions, capers, mushrooms, pepper, Worcestershire sauce, and other seasonings, often presented separately, to be added to taste."
+    document.getElementById("ellipse").style.backgroundColor="#69831e";
+
+    document.getElementById("Pl3").classList.add("default");
+   
+    document.getElementById("Pl4").classList.add("bottom-left")
+  
+    document.getElementById("Pl1").classList.add("bottom-right")
+    
+    document.getElementById("Pl2").classList.add("top-right")
+
+
+
+
+    document.getElementById("Pl1").classList.remove("default");
+   
+    document.getElementById("Pl2").classList.remove("bottom-left")
+  
+    document.getElementById("Pl3").classList.remove("bottom-right")
+    
+    document.getElementById("Pl4").classList.remove("top-right")
+
+    document.getElementById("Pl2").classList.remove("default");
+   
+    document.getElementById("Pl3").classList.remove("bottom-left")
+  
+    document.getElementById("Pl4").classList.remove("bottom-right")
+    
+    document.getElementById("Pl1").classList.remove("top-right")
+
+    document.getElementById("Pl4").classList.remove("default");
+   
+    document.getElementById("Pl1").classList.remove("bottom-left")
+  
+    document.getElementById("Pl2").classList.remove("bottom-right")
+    
+    document.getElementById("Pl3").classList.remove("top-right")
+
+}
+   else
+   {
+    document.getElementById("main").style.backgroundImage="url(img/BG4.png)";
+    document.getElementById("main").style.backgroundColor="#fed7d3";
+    document.getElementById("title").innerHTML="Curti’s Roast";
+    document.getElementById("details").innerHTML="Curtis Stone gives you the perfect pork roast topped with crispy, crunchy crackling. He uses a sharp knife to score the fat at just the right depth for perfect results every time."
+    document.getElementById("ellipse").style.backgroundColor="#db1b15";
+
+    document.getElementById("Pl4").classList.add("default");
+   
+    document.getElementById("Pl1").classList.add("bottom-left")
+  
+    document.getElementById("Pl2").classList.add("bottom-right")
+    
+    document.getElementById("Pl3").classList.add("top-right")
+
+
+
+
+    document.getElementById("Pl1").classList.remove("default");
+   
+    document.getElementById("Pl2").classList.remove("bottom-left")
+  
+    document.getElementById("Pl3").classList.remove("bottom-right")
+    
+    document.getElementById("Pl4").classList.remove("top-right")
+
+    document.getElementById("Pl2").classList.remove("default");
+   
+    document.getElementById("Pl3").classList.remove("bottom-left")
+  
+    document.getElementById("Pl4").classList.remove("bottom-right")
+    
+    document.getElementById("Pl1").classList.remove("top-right")
+
+    document.getElementById("Pl3").classList.remove("default");
+   
+    document.getElementById("Pl4").classList.remove("bottom-left")
+  
+    document.getElementById("Pl1").classList.remove("bottom-right")
+    
+    document.getElementById("Pl2").classList.remove("top-right")
+
+
+}
+}
